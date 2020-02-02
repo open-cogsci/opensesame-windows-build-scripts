1a7d2a20f3919c31307bf4e79d4eea9f3d91743a
S. Mathot <S.Mathot@rug.nl>
2020-02-02 13:34:17 +0100

    Refactor

--- build.py
+++ build.py
@@ -2,26 +2,35 @@
 
 import os
 import sys
+import shutil
 import subprocess
 
 CONDA_FOLDER = r'conda'
 OS_VERSION = '3.3.0a59'
 BUILD_VERSION = '1'
 MAKENSIS = r'..\nsis-3.05\makensis.exe'
-ZIP = r'..\7zip\7z.exe'
-ENV_TARGET = CONDA_FOLDER + r'\envs\opensesame_{version}'
-if '--py2' in sys.argv:
-    BAT_TMPL = r'bat\build-env-py2.bat.tmpl'
+ZIP = r'..\7zip\7za.exe'
+ENV_TARGET = CONDA_FOLDER + r'\opensesame_{version}'
+if '--py27' in sys.argv:
+    BAT_TMPL = r'bat\build-env-py27.bat.tmpl'
     NSI_TMPL = r'nsi\py2.nsi.tmpl'
-    ENV_FOLDER = CONDA_FOLDER + r'\envs\rapunzel-py2\\'
+    ENV_FOLDER = CONDA_FOLDER + r'\rapunzel-py27\\'
     VERSION = OS_VERSION + '-py27-win64-' + BUILD_VERSION
-else:
-    BAT_TMPL = r'bat\build-env.bat.tmpl'
+elif '--py36' in sys.argv:
+    BAT_TMPL = r'bat\build-env-py36.bat.tmpl'
     NSI_TMPL = r'nsi\py3.nsi.tmpl'
-    ENV_FOLDER = CONDA_FOLDER + r'\envs\rapunzel\\'
+    ENV_FOLDER = CONDA_FOLDER + r'\rapunzel-py36\\'
+    VERSION = OS_VERSION + '-py36-win64-' + BUILD_VERSION
+else:
+    BAT_TMPL = r'bat\build-env-py37.bat.tmpl'
+    NSI_TMPL = r'nsi\py3.nsi.tmpl'
+    ENV_FOLDER = CONDA_FOLDER + r'\rapunzel-py37\\'
     VERSION = OS_VERSION + '-py37-win64-' + BUILD_VERSION
 
-
+print('Clearing environments')
+shutil.rmtree(CONDA_FOLDER)
+os.mkdir(CONDA_FOLDER)
+print('Done')
 # First build the environment
 print('Creating build.bat')
 with open(BAT_TMPL) as fd:
@@ -49,6 +58,6 @@ subprocess.run([
     'a',
     '-y',
     'opensesame_{}.zip'.format(VERSION),
-    ENV_TARGET.format(version=VERSION)
+    os.path.abspath(ENV_TARGET.format(version=VERSION))
 ])
 print('Done!')
