efc4115634e805977adc2c483b0fba5ade105e89
smathot <s.mathot@cogsci.nl>
2020-04-24 13:10:45 +0200

    Update for 3.3.0

--- build.py
+++ build.py
@@ -13,7 +13,7 @@ else:
     COMPRESS = '--zip' in sys.argv
 FROZEN = '--frozen' in sys.argv
 CONDA_FOLDER = r'conda'
-OS_VERSION = '3.3.0a73'
+OS_VERSION = '3.3.0'
 BUILD_VERSION = '1'
 MAKENSIS = r'..\nsis-3.05\makensis.exe'
 ZIP = r'..\7zip\7za.exe'
