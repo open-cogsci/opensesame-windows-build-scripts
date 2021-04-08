3b00db8e16427e1b9d763003882046e9aa363581
smathot <s.mathot@cogsci.nl>
2021-04-08 17:16:53 +0200

    Update bat launchers

--- bat/ipython.bat
+++ bat/ipython.bat
@@ -1,5 +1,5 @@
 ECHO Please don't close this window. (Doing so will close IPython.)
 cd /D %~dp0
-SET PATH=%PATH%;%~dp0\Library\bin
-SET PATH=%PATH%;%~dp0\Scripts
+SET PATH=%~dp0\Library\bin;%PATH%
+SET PATH=%~dp0\Scripts;%PATH%
 Scripts\ipython.exe
--- bat/psychopy.bat
+++ bat/psychopy.bat
@@ -1,5 +1,5 @@
 ECHO Please don't close this window. (Doing so will close PsychoPy.)
 cd /D %~dp0
-SET PATH=%PATH%;%~dp0\Library\bin
-SET PATH=%PATH%;%~dp0\Scripts
+SET PATH=%~dp0\Library\bin;%PATH%
+SET PATH=%~dp0\Scripts;%PATH%
 python.exe Lib\site-packages\psychopy\app\psychopyApp.py
