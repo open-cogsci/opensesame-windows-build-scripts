82969b4f78b2a063bd070570862b9e954df15386
smathot <s.mathot@cogsci.nl>
2021-09-02 10:05:46 +0200

    Updates for 3.3.10

--- build-all.bat
+++ build-all.bat
@@ -1,4 +1,3 @@
 python build.py --full --py37 --frozen && ^
 python build.py --full --py27 --frozen && ^
-python build.py --full --py37-megapack --frozen && ^
-python build.py --full --py39-rapunzel
+python build.py --full --py37-megapack --frozen
