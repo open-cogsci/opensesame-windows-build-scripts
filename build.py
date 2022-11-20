56223da0108def73837905ee98538c91555aea1e
Sebastiaan Mathot <s.mathot@cogsci.nl>
2022-11-20 11:36:56 +0100

    Preparing for 3.3.13

--- build.py
+++ build.py
@@ -15,7 +15,7 @@ else:
     COMPRESS = '--zip' in sys.argv
 FROZEN = '--frozen' in sys.argv
 CONDA_FOLDER = r'conda'
-OS_VERSION = '3.3.13a3'
+OS_VERSION = '3.3.13a4'
 RAPUNZEL_VERSION = '0.5.42'
 BUILD_VERSION = '1'
 MAKENSIS = r'..\nsis-3.05\makensis.exe'
